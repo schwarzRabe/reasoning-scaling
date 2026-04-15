9ab1b90aef3206ac068bb27d8281e86ecc6321a0
schwarzRabe <kolotiy.marianna@gmail.com>
2026-04-15 20:54:49 +0300

    Delete rscaling/grpo.py

--- rscaling/grpo.py
+++
@@ -1,9 +0,0 @@
-"""
-GRPO-дообучение лучших SFT-моделей.
-
-    train_grpo(config, reward_fn) -> Path
-        Загружает SFT-checkpoint, запускает TRL GRPOTrainer.
-        reward_fn передаётся снаружи.
-        Полный режим (2000 шагов) для vol 500 и 5000,
-        укороченный (500 шагов) для vol 1000 и 2000.
-"""
